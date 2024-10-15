d25e8c70d6f25740ba5ec085434e6bfbdfa9e99d
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-10-15 22:57:10 +0530

    To clean the Date Perform task as Standardize the Date Format and Populate Property Address Data

--- Data Cleaning/Nashville Housing.sql
+++ Data Cleaning/Nashville Housing.sql
@@ -17,3 +17,24 @@ Add SaleDateConverted Date
 Update PortfolioProject..NashVilleHousing
 Set SaleDateConverted = CONVERT(Date,SaleDate)
 
+-- Populate Property Address Data 
+
+select * 
+From PortfolioProject..NashVilleHousing
+--Where PropertyAddress is null
+order by ParcelID
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortfolioProject..NashVilleHousing as a 
+join PortfolioProject..NashVilleHousing as b
+on a.ParcelID = b.ParcelID
+and a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is null
+
+Update a 
+Set PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortfolioProject..NashVilleHousing as a 
+join PortfolioProject..NashVilleHousing as b 
+on a.ParcelID = b.ParcelID
+and a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is null
